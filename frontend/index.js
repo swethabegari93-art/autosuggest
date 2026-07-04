f128e02703ec89eaccaeb153eeda2a2930e9e2d8
swethabegari93-art <swethabegari93@gmail.com>
2026-07-04 10:22:44 +0530

    my initial commit

--- frontend/index.js
+++ frontend/index.js
@@ -23,3 +23,27 @@ function toggle(){
 }
 
 console.log("Hello from JS");
+function getRandomUser()
+{
+  fetch("https://randomuser.me/api")
+    .then(function (data){
+      return data.json()
+    })
+    .then(function (parsedData){
+      // console.log(parsedData)
+      let gender = parsedData.results[0].gender;
+
+      let first = parsedData.results[0].name.first;
+      let last = parsedData.results[0].name.last;
+      let name = first  + " " + last;
+      let imageUrl = parsedData.results[0].picture.large;
+
+      // gender
+      document.getElementById("card-gender").innerText=gender;
+      // name
+      document.getElementById("card-name").innerText=name
+      // img
+      document.getElementById("img").src=imageUrl
+
+    })
+}
